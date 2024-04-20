c7592b42265a6d9866b430aa1631733c3d157463
AfrinRai <islamrai13@gmail.com>
2024-04-21 00:43:23 +0600

    add each information using card

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,10 @@
+
+const Banner = () => {
+    return (
+        <div>
+            <h2 className="text-center text-5xl my-7 text-green-600 font-bold">Banner page</h2>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/components/Job/Job.jsx
@@ -0,0 +1,41 @@
+import { MdLocationOn } from "react-icons/md";
+import { AiTwotoneDollarCircle } from "react-icons/ai";
+
+const Job = ({job}) => {
+    const {logo, job_title, company_name, remote_or_onsite, location, job_type, salary} = job;
+    return (
+        <div>
+            <div className="card h-96 shadow-xl">
+                <figure><img src={logo} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">{job_title}</h2>
+                    <p>{company_name}</p>
+                    <div>
+                        <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]"> 
+                            {remote_or_onsite}
+                        </button>
+                        <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] mr-4 text-[#7E90FE]">
+                            {job_type}
+                        </button>
+                        <div className="mt-4 flex">
+                            <h2 className="flex items-center mr-4">
+                                <MdLocationOn className="text-2xl mr-2"></MdLocationOn> {location}
+                            </h2>
+                            <h2 className="flex">
+                                <AiTwotoneDollarCircle className="text-2xl mr-2"></AiTwotoneDollarCircle>
+                                {salary}
+                            </h2>
+                        </div>
+                    </div>
+                    <div className="card-actions">
+                        <button className="btn btn-primary bg-gradient-to-r from-indigo-500 to-purple-500 ...">
+                            View Details
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Job;
