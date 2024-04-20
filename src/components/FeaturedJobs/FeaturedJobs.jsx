3f01940e6fb4a4588b0b931bfda8c9d7eabf8929
AfrinRai <islamrai13@gmail.com>
2024-04-21 01:42:22 +0600

    provide show all jobs button functionality

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -5,6 +5,9 @@ const FeaturedJobs = () => {
 
     const [jobs, setJobs] = useState([]);
 
+    // This is not the best way to load show all data :-
+    const [dataLength, setDataLength] = useState(4)
+
     useEffect(() => {
         fetch('jobs.json')
         .then(res => res.json())
@@ -17,8 +20,15 @@ const FeaturedJobs = () => {
                 Explore thousands of job opportunities with all the information you need. Its your future</p>
             <div className="grid grid-cols-2 gap-6">
                 {
-                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                    jobs.slice(0, dataLength).map(job => <Job key={job.id} job={job}></Job>)
                 }
+            </div>
+            <div className={dataLength === jobs.length ? 'hidden' : ''}>
+                <button
+                    onClick={() => setDataLength(jobs.length)}
+                    className="btn btn-primary bg-gradient-to-r from-indigo-500 to-purple-500 ...">
+                    Show all jobs
+                </button>
             </div>
         </div>
     );
